552c44b3aafd28985dde223ab2513f2ba2591216
robertkrimen <robertkrimen@gmail.com>
2008-08-15 01:47:42 -0700

    First pass at a workable version

--- lib/Hash/Param.pm
+++ lib/Hash/Param.pm
@@ -5,52 +5,137 @@ use strict;
 
 =head1 NAME
 
-Hash::Param - The great new Hash::Param!
+Hash::Param - CGI/Catalyst::Request-like parameter-hash accessor/mutator store
 
 =head1 VERSION
 
 Version 0.01
 
+=head1 SYNPOSIS
+
+=head1 DESCRIPTION
+
 =cut
 
 our $VERSION = '0.01';
 
+use Moose;
+use Carp::Clan;
+
+use Hash::Slice;
+
+has parameters => qw/accessor _parameters isa HashRef lazy_build 1/;
+sub _build_parameters {
+    return {};
+}
+
+has _is_rw => qw/is rw default 1/;
+
+sub BUILD {
+    my $self = shift;
+    my $given = shift;
+
+    my $is = $given->{is};
+    if ($is =~ m/^(?:rw|readwrite|writable)$/i) {
+        $self->_is_rw(1);
+    }
+    elsif ($is =~ m/^(?:ro|readonly)$/i) {
+        $self->_is_rw(0);
+    }
+    else {
+        croak "Don't understand this read/write designation: \"$is\"";
+    }
+}
+
+sub parameters {
+    my $self = shift;
+    return $self->params(@_);
+}
+
+sub params {
+    my $self = shift;
+    if (@_) {
+        if (1 == @_ && ref $_[0] eq "HASH") {
+            croak "Unable to modify readonly parameters" unless $self->_is_rw;
+            $self->_parameters($_[0]);
+        }
+        else {
+            my @params = map { scalar $self->option($_) } @_;
+            return wantarray ? @params : \@params;
+        }
+    }
+    else {
+        return wantarray ? keys %{ $self->_parameters } : $self->_is_rw ? $self->_parameters : { %{ $self->_parameters } };
+    }
+}
+
+sub slice {
+    my $self = shift;
+    my $parameters = $self->_parameters;
+    return $self->_is_rw ? Hash::Slice::slice $parameters, @_ : Hash::Slice::clone_slice $parameters, @_;
+}
+
+sub parameter {
+    my $self = shift;
+    return $self->param(@_);
+}
+
+sub get {
+    my $self = shift;
+    return $self->params(@_) if @_ > 1;
+    return $self->param(@_);
+}
+
+sub param {
+    my $self = shift;
+
+    if (@_ == 0) {
+        return keys %{ $self->_parameters };
+    }
+
+    if (@_ == 1) {
+        
+        my $param = shift;
+
+        if (ref $param eq "ARRAY") {
+            return $self->params(@$param);
+        }
+
+        unless (exists $self->_parameters->{$param}) {
+            return wantarray ? () : undef;
+        }
+
+        if (ref $self->_parameters->{$param} eq 'ARRAY') {
+            return (wantarray)
+              ? @{ $self->_parameters->{$param} }
+              : $self->_parameters->{$param}->[0];
+        }
+        else {
+            return (wantarray)
+              ? ($self->_parameters->{$param})
+              : $self->_parameters->{$param};
+        }
+    }
+    elsif (@_ > 1) {
+        my $field = shift;
+        croak "Unable to modify readonly parameter @{[ $field || '' ]}" unless $self->_is_rw;
+        $self->_parameters->{$field} = [@_];
+    }
+}
 
 =head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Hash::Param;
-
-    my $foo = Hash::Param->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
 Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 SOURCE
+
+You can contribute or fork this project via GitHub:
+
+L<http://github.com/robertkrimen/hash-param/tree/master>
+
+    git clone git://github.com/robertkrimen/hash-param.git Hash-Param
 
 =head1 BUGS
 
